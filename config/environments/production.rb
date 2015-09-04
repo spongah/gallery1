454cac5569fb64a86af3410ed405a1ec91f35d1f
spongah <rocksolid801@gmail.com>
2015-09-04 12:57:52 -0600

    adding more config info that i forgot for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'rocksolidgallery.herokuapp.com' }
+  # Mandrill configuration for devise confirmable
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'rocksolidgallery.herokuapp.com' }
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
