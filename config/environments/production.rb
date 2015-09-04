046695f5960ef44d46f62a1cc9007c8de694846e
spongah <rocksolid801@gmail.com>
2015-09-04 12:51:07 -0600

    fixed default host in production environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'gallery2.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rocksolidgallery.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
