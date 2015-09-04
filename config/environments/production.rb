e08a9e375cb8ec6e4ca604c74d08294a3fbafc91
spongah <rocksolid801@gmail.com>
2015-09-04 13:23:53 -0600

    fixed one wrong setting in development environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,13 +14,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Mandrill settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.default_url_options = { :host => 'soundresume.herokuapp.com' }
-
+  config.action_mailer.default_url_options = { :host => 'rocksolidgallery.herokuapp.com' }
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
